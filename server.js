7fa1398c69fd21c47322130f4c66437f9734ac90
codewithabhishekchhapre <abhishekgurjar5020@gmail.com>
2025-06-03 18:09:13 +0530

    comment api code

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 
+
 const server = createServer((req, res) => {
-     const url = parse(req.url, true);
-     const method = req.method;
+     // const url = parse(req.url, true);
+     // const method = req.method;
      
-     if(method === 'GET' && url.pathname === '/api') {
-          console.log("abhi")
-          res.end(JSON.stringify({ message: 'Welcome to the API' }));
-     }
-     else {
-          res.statusCode = 404;
-          res.end(JSON.stringify({ error: 'Not found' }));
-        }
+     // if(method === 'GET' && url.pathname === '/api') {
+     //      console.log("abhi")
+     //      res.end(JSON.stringify({ message: 'Welcome to the API' }));
+     // }
+     // else {
+     //      res.statusCode = 404;
+     //      res.end(JSON.stringify({ error: 'Not found' }));
+     //    }
+
+     res.end(JSON.stringify({
+          message: 'Welcome to the API',
+     }));
+
 });
 
 server.listen(3000, () => {
