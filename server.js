8a74d7d748332c12c3ed26b6553f3c4459730fee
codewithabhishekchhapre <abhishekgurjar5020@gmail.com>
2025-06-03 18:01:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+
+
+const server = createServer((req, res) => {
+     const url = parse(req.url, true);
+     const method = req.method;
+     
+     if(method === 'GET' && url.pathname === '/api') {
+          console.log("abhi")
+          res.end(JSON.stringify({ message: 'Welcome to the API' }));
+     }
+     else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ error: 'Not found' }));
+        }
+});
+
+server.listen(3000, () => {
+     console.log('Server is running on port 3000');
+})
